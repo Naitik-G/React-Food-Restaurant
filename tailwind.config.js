6c3f536f5b091468c42d9a5c1d995d6e8f6a2579
Naitik-G <kumarnaitk@gmail.com>
2025-02-05 09:42:46 +0530

    Initial commit or updated features

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      overflow: {
+        'hidden-x': 'hidden',
+      },
+    },
   },
   plugins: [],
 }
